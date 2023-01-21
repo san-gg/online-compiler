9859089a8f9933c8a4a4213e404de8cac5407828
Sanket Puthane <sanket.puthane1@gmail.com>
2023-01-22 03:42:58 +0530

    Init config with CodeMirror 6

--- ui/src/main/main.component.ts
+++ ui/src/main/main.component.ts
@@ -3,7 +3,13 @@ import {
   ElementRef,
   OnInit,
   ViewEncapsulation,
+  AfterViewInit,
 } from '@angular/core';
+
+import { EditorView } from '@codemirror/view';
+import { cpp } from '@codemirror/lang-cpp';
+import { basicSetup } from 'codemirror';
+import { materialDark } from '@ddietr/codemirror-themes/material-dark';
 
 @Component({
   selector: 'main',
@@ -11,33 +17,44 @@ import {
   styleUrls: ['./main.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
   splitterLayout: string = 'horizontal';
-  codeMirrorOptions: any = {
-    theme: 'material',
-    lineNumbers: true,
-    lineWrapping: false,
-    foldGutter: true,
-    gutters: [
-      'CodeMirror-linenumbers',
-      'CodeMirror-foldgutter',
-      'CodeMirror-lint-markers',
-    ],
-    autoCloseBrackets: true,
-    matchBrackets: true,
-    lint: true,
-    indentUnit: 4,
-    smartIndent: true,
-    //inputStyle: 'contenteditable',
-    dragDrop: true,
-    spellcheck: false,
-    autocorrect: false,
-    autocapitalize: false,
-  };
+  codeEditor: EditorView | null = null;
+  // codeMirrorOptions: any = {
+  //   theme: 'material',
+  //   lineNumbers: true,
+  //   lineWrapping: false,
+  //   foldGutter: true,
+  //   gutters: [
+  //     'CodeMirror-linenumbers',
+  //     'CodeMirror-foldgutter',
+  //     'CodeMirror-lint-markers',
+  //   ],
+  //   autoCloseBrackets: true,
+  //   matchBrackets: true,
+  //   lint: true,
+  //   indentUnit: 4,
+  //   smartIndent: true,
+  //   //inputStyle: 'contenteditable',
+  //   dragDrop: true,
+  //   spellcheck: false,
+  //   autocorrect: false,
+  //   autocapitalize: false,
+  // };
 
   constructor(private _elementRef: ElementRef) {}
 
   ngOnInit(): void {
     this._elementRef.nativeElement.removeAttribute('ng-version');
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this._elementRef.nativeElement);
+    const element: HTMLElement | null = document.getElementById('code-block');
+    console.log(element);
+    this.codeEditor = new EditorView({
+      extensions: [cpp(), basicSetup, materialDark],
+    });
+    element?.append(this.codeEditor.dom);
   }
 }
--- ui/src/main/main.module.ts
+++ ui/src/main/main.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MainComponent } from './main.component';
 import { SplitterModule } from 'primeng/splitter';
-import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [BrowserModule, SplitterModule, CodemirrorModule],
+  imports: [BrowserModule, SplitterModule],
   providers: [],
   bootstrap: [MainComponent],
 })
